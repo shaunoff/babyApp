c5c7b8552e10e5b57a0d377c8ea2b5f7d0c30596
shaun hutch <shaunhutch@shauns-Air.home>
2017-05-07 23:31:11 -0400

    app2

--- imports/client/pages/Download.js
+++ imports/client/pages/Download.js
@@ -2,9 +2,10 @@ import React, { PropTypes } from 'react';
 
 
 const Download = () =>
-  <div className='main-layout'>
-    <a href="itms-services://?action=download-manifest&amp;url=https://obscure-inlet-34461.herokuapp.com/manifest.plist"><img src="https://portal.p3i-inc.com/wp-content/uploads/2015/11/Apple-logo.png" alt="Apple-logo" width="50" height="60" />
-Download
+  <div style={{display: 'flex', alignItems: 'center',margin: '50px',flexDirection: 'column'}}>
+    <h3 style={{color: '#aaa', margin: '20px', fontFamily: "arial"}}>Click the Icon to Download</h3>
+    <a href="itms-services://?action=download-manifest&amp;url=https://obscure-inlet-34461.herokuapp.com/manifest.plist"><img src="/apple.png" alt="Apple-logo" width="100" height="100" />
+
 </a>
 
   </div>
