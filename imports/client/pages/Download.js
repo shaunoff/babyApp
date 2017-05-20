48b80e416ff4f7c8e81ca4a9bfadd16d48eb8cbb
shaun hutch <shaunhutch@shauns-Air.home>
2017-05-20 17:28:51 -0400

    android

--- imports/client/pages/Download.js
+++ imports/client/pages/Download.js
@@ -4,9 +4,9 @@ import React, { PropTypes } from 'react';
 const Download = () =>
   <div style={{display: 'flex', alignItems: 'center',margin: '50px',flexDirection: 'column'}}>
     <h3 style={{color: '#aaa', margin: '20px', fontFamily: "arial"}}>Click the Icon to Download</h3>
-    <a href="itms-services://?action=download-manifest&amp;url=https://obscure-inlet-34461.herokuapp.com/manifest.plist"><img src="/apple.png" alt="Apple-logo" width="100" height="100" />
-
-</a>
+    <a href="itms-services://?action=download-manifest&amp;url=https://obscure-inlet-34461.herokuapp.com/manifest.plist"><img src="/apple.png" alt="Apple-logo" width="100" height="100" /></a>
+    <h3 style={{color: '#aaa', margin: '40px 20px 20px 40px', fontFamily: "arial"}}>Click the Icon to Download</h3>
+    <a href="app_debug.apk" download="app_debug.apk"><img src="/android3.png" alt="android-logo" width="120" height="120" download/></a>
 
   </div>
 
