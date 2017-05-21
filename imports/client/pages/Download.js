1cc4081856905f82108302e5a36e632e4cdd24b8
shaun hutch <shaunhutch@shauns-Air.home>
2017-05-21 12:03:44 -0400

    android3

--- imports/client/pages/Download.js
+++ imports/client/pages/Download.js
@@ -6,7 +6,7 @@ const Download = () =>
     <h3 style={{color: '#aaa', margin: '20px', fontFamily: "arial"}}>Click the Icon to Download</h3>
     <a href="itms-services://?action=download-manifest&amp;url=https://obscure-inlet-34461.herokuapp.com/manifest.plist"><img src="/apple.png" alt="Apple-logo" width="100" height="100" /></a>
     <h3 style={{color: '#aaa', margin: '40px 20px 20px 40px', fontFamily: "arial"}}>Click the Icon to Download</h3>
-    <a href="https://obscure-inlet-34461.herokuapp.com/app_debug.apk" download="app_debug.apk" download><img src="/android3.png" alt="android-logo" width="120" height="120" /></a>
+    <a href="https://obscure-inlet-34461.herokuapp.com/app_release.apk" download="app_release.apk" download><img src="/android3.png" alt="android-logo" width="120" height="120" /></a>
 
   </div>
 
